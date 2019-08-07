f84a34c9e9c40fbe49766a1c82014674e481eebe
Sarfulah Stewart <fu.stew@yahoo.com>
2019-08-07 14:13:56 -0400

    layout for pages are set

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,22 +1,100 @@
 var friends = [
     {
-    name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores:[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  }, {
-      //another friend object
-  }
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Nai Lang",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            4,
+            1,
+            5,
+            3,
+            2,
+            3,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Hunter",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            4,
+            3,
+            4,
+            5,
+            2,
+            4,
+            1,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Idris",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            4,
+            4,
+            4,
+            3,
+            3,
+            5,
+            5,
+            4,
+            4
+        ]
+    },
+    {
+        name: "Curry",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            3,
+            1,
+            1,
+            2,
+            1,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Mr. Cooper",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            2,
+            4,
+            4,
+            5,
+            2,
+            1,
+            3,
+            2,
+            5,
+            5
+        ]
+    }
 ]
 
 module.exports = friends;
--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,15 +1,22 @@
 var friends = require('../data/friends.js');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
 
-app.get('/api/friends', function(req, res) {
-    res.json(friends);
-});
+    app.get('/api/friends', function (req, res) {
+        res.json(friends);
+    });
 
-app.post('/api/friends', function(req, res) {
-    friends.push(req.body);
-    res.json(true);
-})
+    app.post('/api/friends', function (req, res) {
+        if (friends.length < 5) {
+            friends.push(req.body);
+            res.json(true);
+        }
+    });
 
-}
+    app.post("/api/clear", function (req, res) {
+        friends.length = 0;
+
+        res.json({ ok: true });
+    });
+};

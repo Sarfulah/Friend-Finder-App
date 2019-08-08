479eff6828cb561385c8945b23aff0db2c199b96
Sarfulah Stewart <fu.stew@yahoo.com>
2019-08-08 15:10:04 -0400

    the game works, need photo

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -8,15 +8,31 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
-        if (friends.length < 5) {
-            friends.push(req.body);
-            res.json(true);
+        
+        var difference;
+        var bestMatch = {
+            name: "", 
+            photo: "", 
+            difference: 1000
         }
-    });
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i].name);
+            var singleFriend = friends[i];
+            difference = 0;
 
-    app.post("/api/clear", function (req, res) {
-        friends.length = 0;
-
-        res.json({ ok: true });
+            for (var j = 0; j < singleFriend.scores.length; j++){
+                console.log("This is the single friend's scores: " + singleFriend.scores[j])
+                difference += Math.abs(parseInt(singleFriend.scores[j]) - parseInt(req.body.scores[j]))
+                console.log("This is the difference: " + difference);
+            }
+            if (difference <= bestMatch.difference){
+                bestMatch.name = singleFriend.name, 
+                bestMatch.photo = singleFriend.photo, 
+                bestMatch.difference = difference
+            }
+        }
+            friends.push(req.body);
+            res.json(bestMatch);
+        
     });
 };
